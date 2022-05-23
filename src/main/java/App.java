06c4f06fc66cfaf9e54de416822f9edaa212a4c6
Jakub Rogowski <jakerogowski33@gmail.com>
2022-05-23 13:18:22 +0200

    Text file from user input until hash occurs.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,30 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws IOException {
 
-        Path appPath = Path.of("src", "main", "java", "App.java");
-        Path resourcesPath = Path.of("src", "main", "resources", "classApp.txt");
+        Path resourcesPath = Path.of("src", "main", "resources", "file.txt");
+        Scanner sc = new Scanner(System.in);
+        StringBuilder userInput = new StringBuilder();
 
-        String copyText = Files.readString(appPath);
-        System.out.println(copyText);
+        System.out.println("User input: ");
 
-        Files.writeString(resourcesPath, copyText);
+        while(!userInput.toString().contains("#")) {
+            userInput.append(sc.nextLine());
+            userInput.append("\r\n");
+        }
+
+        userInput.delete(userInput.indexOf("#"), userInput.length());
+
+        String input = userInput.toString();
+
+        System.out.println(input);
+        System.out.println("Hello");
+
+        Files.writeString(resourcesPath, input);
 
     }
 }
