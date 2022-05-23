083dfcfa17a9bd83481e9ca360d09b4439e4a601
Jakub Rogowski <jakerogowski33@gmail.com>
2022-05-23 12:01:07 +0200

    Files and paths revision.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,12 @@
+import java.nio.file.Path;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Json training");
+
+        Path myPath = Path.of("src", "main", "java", "resources", "file.txt");
+
+        System.out.println(myPath);
+        System.out.println(myPath.toAbsolutePath());
+
     }
 }
