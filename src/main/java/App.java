1cc35ca230b8747ab4c512c9ca90ce24f16d8eb4
Jakub Rogowski <jakerogowski33@gmail.com>
2022-05-23 14:16:03 +0200

    JSON start

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,32 +1,5 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Scanner;
-
 public class App {
-    public static void main(String[] args) throws IOException {
-
-        Path resourcesPath = Path.of("src", "main", "resources", "file.txt");
-        Scanner sc = new Scanner(System.in);
-        StringBuilder userInput = new StringBuilder();
-
-        userInput.append(Files.readString(resourcesPath));
-        System.out.println(userInput);
-
-        System.out.println("User input: ");
-
-        while(!userInput.toString().contains("#")) {
-            userInput.append(sc.nextLine());
-            userInput.append("\r\n");
-        }
-
-        userInput.delete(userInput.indexOf("#"), userInput.length());
-
-        String input = userInput.toString();
-
-        System.out.println(input);
-
-        Files.writeString(resourcesPath, input);
-
+    public static void main(String[] args) {
+        System.out.println("JSON start");
     }
 }
